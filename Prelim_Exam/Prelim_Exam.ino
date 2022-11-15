de0f216cef0730c3b4d4593d924306c647806a6c
PaoGon <pickelse25@gmail.com>
2022-11-15 19:36:29 +0800

    added LCDI2c functions:
    change project structure

--- Prelim_Exam/Prelim_Exam.ino
+++ Prelim_Exam/Prelim_Exam.ino
@@ -3,6 +3,7 @@
 #include <NewPing.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "src/barGraph/barGraph.h"
 
 
 #define sensor 2            // S, vcc, gnd
@@ -18,6 +19,7 @@ unsigned long level_prev_time = 0;
 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+BarGraph barGraph(&lcd);
 
 byte cup_threshold = 7;
 
@@ -31,16 +33,23 @@ void displayTemp(float tempc, float humid){
 
 void setup() {
     Serial.begin(9600);
+
     pinMode(sensor, INPUT);
+
     dht.begin();
+
     lcd.init();
+
+    barGraph.begin();
+
     lcd.backlight();
     lcd.setCursor(0, 0);
     lcd.print("  Prelim Exam ");
     lcd.setCursor(0, 1);
     lcd.print("   A.G.G.N.O.  ");
+    lcd.clear();
     delay(2000);
-  }
+}
 
 void loop() {
     unsigned long curr_time = millis();
@@ -65,6 +74,11 @@ void loop() {
       Serial.print(sonar_reading); 
       Serial.println(" cm");
 
+      barGraph.printBar(EEPROM[2], sonar_reading);
+      lcd.setCursor(0, 1);
+      lcd.print("Total cups:");
+      lcd.print(EEPROM[2]);
+
       // checks if the current reading is >= the previous reading
       if(sonar_reading != EEPROM[0]){ 
         /*
@@ -80,7 +94,8 @@ void loop() {
         // if curr_reading > water_level: increment 
         if(sonar_reading > EEPROM.read(0)){ 
           // EEPROM.write(1, water_level + sonar_reading);
-          EEPROM[1] = water_level + sonar_reading;
+          byte current_intake = sonar_reading - EEPROM[0];
+          EEPROM[1] = current_intake + water_level;
           
           Serial.print("Water Level Intake: ");
           Serial.println(EEPROM[1]);
@@ -93,14 +108,15 @@ void loop() {
           EEPROM[1] = water_diff; 
 
           byte cup_counter = EEPROM[2];
-          EEPROM.write(2, cup_counter + 1);
-          Serial.print("Total Intake: ");
+          EEPROM[2] = cup_counter + 1;
+          Serial.print("Number of Cups: ");
           Serial.println(EEPROM[2]);
+
+          EEPROM[0] = 0;
         }
         EEPROM.write(0, sonar_reading);   //save the current reading to EEPROM address 0
 
      }
-      
 
       level_prev_time = level_curr_time;
     }
---
+++ Prelim_Exam/src/barGraph/barGraph.h
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+#include <LiquidCrystal_I2C.h>
+
+
+class BarGraph{
+  public:
+    BarGraph(LiquidCrystal_I2C*);
+    void begin();
+    void printBar(int position, int val);
+    void setPrevBar(int val);
+
+  private:
+    LiquidCrystal_I2C* _lcd;
+};
