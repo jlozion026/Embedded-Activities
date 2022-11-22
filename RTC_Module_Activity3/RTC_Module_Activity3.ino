df0187a97783d96959615952aa49b86735d2c7dc
jlozion026 <jlozion@gmail.com>
2022-11-22 14:19:05 +0800

    Added Alarm Function

--- RTC_Module_Activity3/RTC_Module_Activity3.ino
+++ RTC_Module_Activity3/RTC_Module_Activity3.ino
@@ -2,12 +2,12 @@
 #include <ThreeWire.h>
 #include <RtcDS1302.h>
 #include <Keypad.h>
-
-
+// int year, month, day, hour, minute, second;
+int hour, minute, second;
 //0x3F or 0x27
 LiquidCrystal_I2C lcd(0x27, 16, 2);   //LCD Object
 
-ThreeWire myWire(7, 6, 8);        // DAT, CLK, RST
+ThreeWire myWire(13, 10, 12);        // DAT, CLK, RST
 RtcDS1302<ThreeWire> Rtc(myWire);    // RTC Object
 
 const unsigned long event_interval = 1000;
@@ -15,21 +15,59 @@ unsigned long prev_time = 0;
 
 
 const byte ROWS = 4; //four rows
-const byte COLS = 3; //four columns
+const byte COLS = 4; //four columns
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'},
-  {'7', '8', '9'},
-  {'*', '0', '#'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 
 
-byte row_pins[ROWS] = {6, 7, 8, 9}; //connect to the row pinouts of the keypad
-byte col_pins[COLS] = {3, 4, 5}; //connect to the column pinouts of the keypad
+byte row_pins[ROWS] = {9, 8, 7, 6};
+byte col_pins[COLS] = {5, 4, 3, 2};
 
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), row_pins, col_pins, ROWS, COLS);
+const byte bzz_pin = 11;
+
+void buzz(){
+    for(byte i = 0; i <= 10; i++){
+        for(int buzz = 0; buzz < 2; buzz++){
+            tone(bzz_pin, 1000);
+            delay(200);
+            noTone(bzz_pin);
+            delay(200);
+        }
+        delay(200);
+    }
+}
+
+bool alarm(){
+  RtcDateTime now = Rtc.GetDateTime();
+  if(now.Hour() == hour &&
+     now.Minute() == minute &&
+     now.Second() == second
+  ){
+    return true;
+  }
+  
+  //String dateFormat = "Curr Date " + String(now.Year()) + "/" + String(now.Month()) + "/" + String(now.Day());
+  String timeFormat = "Curr Time " + String(now.Hour()) + "/" + String(now.Minute()) + "/" + String(now.Second());
+
+  //String inputDate = "Input Date " + String(year) + "/" + String(month) + "/" + String(day);
+  String inputTime = "Input Time " + String(hour) + "/" + String(minute) + "/" + String(second);
+
+
+  //Serial.println(dateFormat);
+  Serial.println(timeFormat);
+
+  //Serial.println(inputDate);
+  Serial.println(inputTime);
+  return false;
+  
+}
 
 
 int getInput(){
@@ -60,6 +98,9 @@ void setup ()
   lcd.clear();
 
   Rtc.Begin();
+  // RtcDateTime currentTime = RtcDateTime(__DATE__ , __TIME__);
+  // Rtc.SetDateTime(currentTime);
+  pinMode(bzz_pin, OUTPUT);
 
 }
 
@@ -85,44 +126,50 @@ void loop () {
     lcd.print(":");
     lcd.print(now.Minute());
     lcd.print(":");
-    lcd.println(now.Second());
+    lcd.print(now.Second());
     prev_time = curr_time;
 
     char c = customKeypad.getKey();
     if (c == '*') {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Enter Year: ");
-      int year = getInput();
+      // lcd.clear();
+      // lcd.setCursor(0, 0);
+      // lcd.print("Enter Year: ");
+      // year = getInput();
 
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Enter Month: ");
-      int month = getInput();
+      // lcd.clear();
+      // lcd.setCursor(0, 0);
+      // lcd.print("Enter Month: ");
+      // month = getInput();
 
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Enter DayOfWeek: ");
-      int day = getInput();
+      // lcd.clear();
+      // lcd.setCursor(0, 0);
+      // lcd.print("Enter DayOfWeek: ");
+      // day = getInput();
 
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Enter Hours: ");
-      int hour = getInput();
+      hour = getInput();
 
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Enter Minutes: ");
-      int minute = getInput();
+      minute = getInput();
 
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Enter Seconds: ");
-      int second = getInput();
+      second = getInput();
 
-      RtcDateTime newTime = RtcDateTime(year, month, day, hour, minute, second);
-      Rtc.SetDateTime(newTime);
+      // RtcDateTime newTime = RtcDateTime(year, month, day, hour, minute, second);
+      // Rtc.SetDateTime(newTime);
 
     }
-  }
+      if(alarm()){
+        Serial.println("Alarm");
+        buzz();
+      } else {
+        Serial.println("No Alarm");
+    }
+  } 
 }
