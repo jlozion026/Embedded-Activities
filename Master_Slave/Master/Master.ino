a805daea60b8476c021fb992de705293390f8a5f
jlozion026 <jlozion@gmail.com>
2022-11-30 22:12:48 +0800

    Added Third Slave

--- Master_Slave/Master/Master.ino
+++ Master_Slave/Master/Master.ino
@@ -2,11 +2,14 @@
 
 const int pot_pin1 = A0;
 const int pot_pin2 = A1;
+const int pot_pin3 = A2;
 
-const unsigned long event_interval = 1000;
+const unsigned long event_interval = 50;
 unsigned long prev_time = 0;
 
-int prev_reading_1, prev_reading_2;
+int prev_reading_1 = 0; 
+int prev_reading_2 = 0;
+int prev_reading_3 = 0;
 
 void transmit(byte adr, long data){
     Wire.beginTransmission(adr);                                                                             
@@ -14,8 +17,16 @@ void transmit(byte adr, long data){
     Wire.endTransmission();                                                  
 }
 
+void format_data(int sm, int angle){
+    char data[50];
+    sprintf(data, "Curr: %d,\n Prev: %d.", angle, sm);
+    Serial.println(data);
+}
+
+
 void setup(){
   Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop(){
@@ -26,16 +37,33 @@ void loop(){
 
     int curr_reading_1 = analogRead(pot_pin1);
     int curr_reading_2 = analogRead(pot_pin2);
+    int curr_reading_3 = analogRead(pot_pin3);
+    
 
     if(curr_reading_1 != prev_reading_1){
       int data = map(curr_reading_1, 0, 1023, 0, 180);
       transmit(1, data);
+      //format_data(curr_reading_1, prev_reading_1);
 
+      prev_reading_1 = curr_reading_1;
     }
-    else{
+
+    if(curr_reading_2 != prev_reading_2){
       int data = map(curr_reading_2, 0, 1023, 0, 180);
       transmit(2, data);
+      //format_data(curr_reading_2, prev_reading_2);
+    
+      prev_reading_2 = curr_reading_2;
     }
+
+    if(curr_reading_3 != prev_reading_3){
+      int data = map(curr_reading_3, 0, 1023, 0, 180);
+      transmit(3, data);
+      format_data(curr_reading_3, prev_reading_3);
+
+      prev_reading_3 = curr_reading_3;
+    }
+
     prev_time = curr_time;
   }
 }
---
+++ Master_Slave/Slave3/Slave3.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+#include <Wire.h>
+
+Servo servo;
+
+#define ADDR 3
+
+
+void format_data(int sm, int angle){
+    char data[50];
+    sprintf(data, "angle: %d,\n press: %d.", angle, sm);
+    Serial.println(data);
+}
+
+void rotate(int Press){
+  int angle = Wire.read();
+  servo.write(angle);
+
+  format_data(Press, angle);
+}
+
+void setup(){
+  Wire.begin(ADDR);
+  servo.attach(9);
+  Wire.onReceive(rotate);
+
+  Serial.begin(9600);
+}
+
+void loop(){
+  delay(1000);
+}
+
